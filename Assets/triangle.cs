dffd14bd50297fb06abc8f968d14c97ef0c13ad8
troysan-starloop <troy.santiago@starloopstudios.com>
2022-06-11 17:30:07 +0800

    test draw vertices

--- Assets/triangle.cs
+++ Assets/triangle.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class triangle : MonoBehaviour
 {
-    private Vector3[] _vertices = new Vector3[3];
+    private Vector3[] _vertices;
     private int[] _triangles = new int[3];
     private Vector2[] _uv = new Vector2[3];
 
-    // Start is called before the first frame update
-    void Start(){
-        DefineMesh();
+    private Vector3 _chunkDimension = new Vector3 (4,4,4);
+
+// Start is called before the first frame update
+void Start(){
+        DefineChunkShape();
+    }
+
+    void DefineChunkShape(){
+        var vertCount = 125;
+        for (int i = 0; i < vertCount; i++)
+        {
+            for (int x = 0; x < _chunkDimension.x; x++)
+            {
+                for (int y = 0; y < _chunkDimension.y; y++)
+                {
+                    for (int z = 0; z < _chunkDimension.z; z++)
+                    {
+                        _vertices[i] = new Vector3( x, y, z );
+                    }
+                }
+            }
+        }
     }
 
     void DefineMesh(){
@@ -19,8 +38,8 @@ public class triangle : MonoBehaviour
 
     void OnDrawGizmos() {
         Gizmos.color = Color.blue;
-        for (int i = 0; i < 3; i++){
-            Gizmos.DrawSphere(Vector3.zero + new Vector3(0,i,0), 0.05f);
+        for (int i = 0; i < _vertices.Length; i++){
+            Gizmos.DrawSphere(_vertices[i], 0.05f);
         }
     }
 }
