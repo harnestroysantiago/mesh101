8d87fbff567811200cada0522f8833a05e3376d5
troysan-starloop <troy.santiago@starloopstudios.com>
2022-06-12 09:48:40 +0800

    -vertices adjusted, triangle index needs adjustment

--- Assets/_Scripts/GenerateChunk.cs
+++ Assets/_Scripts/GenerateChunk.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateChunk : MonoBehaviour
@@ -8,8 +10,8 @@ public class GenerateChunk : MonoBehaviour
     private static Vector3Int _chunkDimension = new Vector3Int(3,3,3);
     private int _terrainHeight = 2;
     private BlockDto[,,] _block = new BlockDto[_chunkDimension.x,_chunkDimension.y,_chunkDimension.z];
-    private Vector3[] _vertices;
-    private List<int> _triangles= new List<int>();
+    private List<Vector3> _vertices = new List<Vector3>();
+    private List<int> _triangles = new List<int>();
 
     private Mesh _mesh;
     private MeshFilter _meshFilter;
@@ -22,16 +24,31 @@ public class GenerateChunk : MonoBehaviour
         
         GenerateBlockData();
         
-        GenerateMesh(_mesh);
+        StartCoroutine(GenerateMeshes(_mesh));
+    }
+
+    IEnumerator GenerateMeshes(Mesh mesh)
+    {
+        for (int x = 0; x < _chunkDimension.x; x++)
+        {
+            for (int y = 0; y < _chunkDimension.y; y++)
+            {
+                for (int z = 0; z < _chunkDimension.z; z++)
+                {
+                    GenerateMesh(mesh,x,y,z);
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
     }
 
     // this works for one chunk, we need to generate the whole chunk data and render them.
-    private void GenerateMesh(Mesh mesh)
+    private void GenerateMesh(Mesh mesh, int x, int y, int z)
     {
-        Vector3Int blockLoc = new Vector3Int(0, 0, 0);
+        Vector3Int blockLoc = new Vector3Int(x, y, z);
         BlockDto block = _block[blockLoc.x, blockLoc.y, blockLoc.z];
         
-        _vertices = new Vector3[]
+        Vector3[] vertices = new Vector3[]
         {
             new (0, 0, 0),
             new (0, 1, 0),
@@ -44,11 +61,11 @@ public class GenerateChunk : MonoBehaviour
             new (1, 0, 1)
         };
         
-        // this updates the offset of the chunk, so we always go on the
+        // this updates the offset of the chunk vertices, so we always go on the
         // blockLoc + the vertex to give it a quad on each face
         for (int i = 0; i < 8; i++)
         {
-            _vertices[i] += blockLoc;
+            vertices[i] += blockLoc;
         }
         
         //Bottom [0] = AHE,ADH || 074 , 037
@@ -57,9 +74,9 @@ public class GenerateChunk : MonoBehaviour
         //Right [3] = HDC,HCG || 732 , 726
         //Front [4] = EHG,EGF || 476 , 465
         //Back [5] = DAB,DBC || 301 , 312
+        List<int> triangles = new List<int>();
         
         // the triangles indexes needs to be adjusted as well, based on how many we have....
-        // todo: adjust based on how many we have
         for (int i = 0; i < 6; i++)
         {
             if (block.Side[i] == true)
@@ -67,33 +84,46 @@ public class GenerateChunk : MonoBehaviour
                 switch (i)
                 {
                     case (int)Enums.BlockSide.Bottom:
-                        _triangles.AddRange(new int[]{0,7,4,0,3,7});
+                        triangles.AddRange(new int[]{0,7,4,0,3,7});
                         break;
                     case (int)Enums.BlockSide.Top:
-                        _triangles.AddRange(new int[]{2,1,5,2,5,6});
+                        triangles.AddRange(new int[]{2,1,5,2,5,6});
                         break;
                     case (int)Enums.BlockSide.Left:
-                        _triangles.AddRange(new int[]{0,4,5,0,5,1});
+                        triangles.AddRange(new int[]{0,4,5,0,5,1});
                         break;
                     case (int)Enums.BlockSide.Right:
-                        _triangles.AddRange(new int[]{7,3,2,7,2,6});
+                        triangles.AddRange(new int[]{7,3,2,7,2,6});
                         break;
                     case (int)Enums.BlockSide.Front:
-                        _triangles.AddRange(new int[]{4,7,6,4,6,5});
+                        triangles.AddRange(new int[]{4,7,6,4,6,5});
                         break;
                     case (int)Enums.BlockSide.Back:
-                        _triangles.AddRange(new int[]{3,0,1,3,1,2});
+                        triangles.AddRange(new int[]{3,0,1,3,1,2});
                         break;
                 }
             }
         }
 
+        // this updates the offset of the chunk triangles, so we always go on the
+        // lenght + the vertex index to give it a quad on each face
+        for (int i = 0; i < 6; i++)
+        {
+            //Debug.Log("index " + i + " = "+triangles[i]);
+            
+        }
+        
+        // add to existing _vertex and _triangles
+        _vertices.AddRange(vertices);
+        _triangles.AddRange(triangles);
+
+        var vertArray = _vertices.ToArray();
         var triArray = _triangles.ToArray();
         
         mesh.Clear();
         
         mesh.name = "mesh...illicious";
-        mesh.vertices = _vertices;
+        mesh.vertices = vertArray;
         mesh.triangles = triArray;
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
